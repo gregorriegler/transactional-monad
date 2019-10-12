25214962475209967a395efd7bf63364e5d8401f
Gregor Riegler <gregor.riegler@ebcont.com>
2019-10-13 00:20:33 +0200

    initial commit

---
+++ src/test/java/TestTransactional.java
@@ -0,0 +1,73 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+class TestTransactional {
+
+    private final Transaction transaction = mock(Transaction.class);
+    private final SomeRepository repository = mock(SomeRepository.class);
+    private final InOrder inOrder = Mockito.inOrder(transaction, repository);
+
+    @Test void
+    should_execute_function_within_a_transaction() {
+        Transactional.of(repository::update).run(transaction);
+
+        inOrder.verify(transaction).begin();
+        inOrder.verify(repository).update();
+        inOrder.verify(transaction).commit();
+    }
+
+    @Test void
+    should_rollback_on_exception() {
+        RuntimeException expectedException = new RuntimeException();
+        doThrow(expectedException).when(repository).update();
+
+        assertThatThrownBy(() ->
+            Transactional.of(repository::update).run(transaction)
+        ).isSameAs(expectedException);
+
+        inOrder.verify(transaction).begin();
+        inOrder.verify(repository).update();
+        inOrder.verify(transaction).rollback();
+    }
+
+}
+
+class Transactional {
+    private final Runnable runnable;
+
+    private Transactional(Runnable runnable) {
+        this.runnable = runnable;
+    }
+
+    static Transactional of(Runnable runnable) {
+        return new Transactional(runnable);
+    }
+
+    void run(Transaction transaction) {
+        try {
+            transaction.begin();
+            runnable.run();
+            transaction.commit();
+        } catch (RuntimeException e) {
+            transaction.rollback();
+            throw e;
+        }
+    }
+}
+
+interface Transaction {
+    void begin();
+
+    void commit();
+
+    void rollback();
+}
+
+interface SomeRepository {
+    void update();
+}
